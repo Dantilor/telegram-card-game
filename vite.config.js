99c9c826b1c77e2a78951e7e46b7048cb5c0a9cf
Даниил Кулешов <daniilkulesov@Mac-mini-Daniil.local>
2026-01-30 16:22:58 +0300

    fix: telegram webview legacy build

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-export default defineConfig({
-    base: '/telegram-card-game/',
-    plugins: [react()],
-    define: {
-        __BUILD_TIME__: JSON.stringify(new Date().toISOString()),
-    },
-});

99dc6a081186f6ac566aa0ac3204c7be7e9b041a
Dantilor <163985061+Dantilor@users.noreply.github.com>
2026-06-15 23:54:48 +0300

    Add ridiculous auction page entry

---
+++ src/pages/RidiculousAuctionLayout.tsx
@@ -0,0 +1,7 @@
+import RidiculousAuctionGame from '../games/ridiculous-auction/RidiculousAuctionGame'
+
+function RidiculousAuctionLayout() {
+  return <RidiculousAuctionGame />
+}
+
+export default RidiculousAuctionLayout
